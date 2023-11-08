948266946da46ce131b9a7db6d670708be43a966
Hyungjoon Cho <selcon1996@gmail.com>
2023-11-08 21:58:52 +0900

    BOJ_실버3_먹을 것인가 먹힐 것인가 - 간만에 알고리즘을 풀어서 뇌가 다 굳은 것 같음. 재활 드가자

---
+++ Week 1/BOJ_실버 3_먹을 것인가 먹힐 것인가/hyungjoon.py
@@ -0,0 +1,33 @@
+'''
+문제 : 먹을 것인가 먹힐 것인가
+난이도 : 실버 3
+소요 시간 : 
+'''
+import sys
+se = sys.stdin.readline
+t = int(se().rstrip())
+
+for _ in range(t):
+    a, b = se().split()
+    a = list(map(int, se().split()))
+    b = list(map(int, se().split()))
+
+    # 이분탐색을 위한 정렬
+    a.sort()
+    b.sort()
+    answer = 0
+
+    for i in a:
+        result = -1
+        l, r = 0, len(b)-1
+        while l <= r:
+            mid = (l+r) // 2
+            if i > b[mid]:
+                result = mid
+                l = mid + 1
+            elif i <= b[mid]:
+                r = mid - 1
+        answer += result + 1
+    
+    print(answer)
+        
