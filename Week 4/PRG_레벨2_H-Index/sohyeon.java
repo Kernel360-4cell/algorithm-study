2085ea189dfda732d35baa3b737f3ab82f821560
anso33 <hyeon131812@gmail.com>
2023-12-05 20:06:47 +0900

    PRG_레벨2_H-index

--- Week 4/PRG_레벨2_H-Index/sohyeon.java
+++ Week 4/PRG_레벨2_H-Index/sohyeon.java
@@ -8,7 +8,10 @@ public class pg_42747 {
 		Arrays.sort(citations);
 		for (int i = 0; i < citations.length; i++) {
 			int h = citations.length - i;
+			// h는 인덱스이면서 h번 이상 인용된 논문이 h편 이상이어야 한다.
 			if (citations[i] >= h) {
+				// citation을 오름차순으로 정렬했으므로, h번 이상 인용된 논문이 h편 이상이면 h가 최대값이다.
+				// 다시 말하면 h번 인덱스(뒤에서부터 가는거임)로부터 h만큼 떨어진 인덱스가 적어도 h이상이어야 h번 이상 인용된 논문이 h편 이상이게 되는 것이다.
 				answer = h;
 				break;
 			}
